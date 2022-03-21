61e4a5fe3df02e69c404b4c5383b32bd16c2654b
t-vanderwolk <97120854+t-vanderwolk@users.noreply.github.com>
2022-03-21 09:39:32 -0700

    test

--- Develop/script.js
+++ Develop/script.js
@@ -1,119 +1,75 @@
-// Assignment Code
+document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword());
 
-let generateBtn = document.querySelector("#generate");
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let sChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+let lChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let uChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+let confirmLength = "";
+let confirmC;
+let confirmNC;
+let confirmU;
+let confirmL;
 
-//let passwordLength =""
-let nums =['0','1','2','3','4','5','6','7','8','9'];
-let sChar=['!','@','#','$','%','^','&','*','(',')'];
-let lChar=['a','b','c','d','e','f','g','h','i','j','k','l',,'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let uChar=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let outPassword = [];
-let inputValue=[];
-let passwordLength=[];
-
+// Prompt to confirm the character length
 function generatePassword() {
-  let password = passwordMachine();
-  let passwordText = document.querySelector("#password");
-  passwordText.value = password;
-};
-
-function passwordMachine () {
-  let randomPassword = '';
-  for (let i = 0; i < passwordLength; i++) {
-  let randomNumber = Math.floor(Math.random() * passwordLength.length);
-  randomPassword = randomPassword + userValue[randomNumber]
-  return randomPassword;
-};
-
-function prompts(){
-  inputValue = [];
-  passwordLength = prompt("Please enter a length of at least 8 characters and no more than 128 characters.");
-  if(isNaN(passwordLength)|| passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a valid response");
-    return false;
-  }
-  if (confirm("Would you like your password to include uppercase characters?")) {
-    inputValue = inputValue.concat(uChar);
-  }
-  if (confirm("Would you like your password to include lowercase characters?")) {
-  inputValue= inputValue.concat(lChar);
-  }
-  if (confirm("Would you like your password to include numeric characters?")) {
-    inputValue= inputValue.concat(nums);
-  }
-  if (confirm("Would you like your password to include special characters?")) {
-    inputValue= inputValue.concat(sChar);
-  }
-
-console.log(userValue)
-  return true;
+  let confirmLength = (confirm("Please enter a length of at least 8 characters and no more than 128 characters."));
 }
-generatePassword();
-passwordMachine();
-prompts();
-//  prompt2 = confirm(("Would you like your password to include uppercase characters?"));
-//   prompt3 = confirm(("Would you like your password to include lowercase characters?"));
-//  prompt4 = confirm(("Would you like your password to include numeric characters?"));
-// prompt5 = confirm(("Would you like your password to include special characters?"));
 
+  // Loop if answer is outside of the parameters 
+  if(confirmLength <= 8 || confirmLength >= 128) {
+      alert("Password length must be between 8-128 characters Try again");
+      let confirmLength = (prompt("Please enter a length of at least 8 characters and no more than 128 characters"));
+      } 
 
-  
+      alert(`Your password will have ${confirmLength} characters`);
 
+    confirmC = confirm("Click OK to include special characters");
+    confirmNC = confirm("Click OK to include numeric characters");    
+    cconfirmL = confirm("Click OK to include lowercase characters");
+    confirmU = confirm("Click OK to include uppercase characters");
+      // Loop if answer is not OK
+      if(confirmU === false && confirmL === false && confirmC === false && confirmNC === false) {
+        alert("You must choose at least one parameter");
+        let confirmC = confirm("Click OK to confirm if you would like to include special characters");
+        let confirmNC = confirm("Click OK to confirm if you would like to include numeric characters");    
+        let cconfirmL = confirm("Click OK to confirm if you would like to include lowercase characters");
+        let confirmU = confirm("Click OK to confirm if you would like to include uppercase characters");   
+    } 
 
+      let passwordCharacters =[];
+    
+    if (confirmC) {
+      passwordCharacters = passwordCharacters.concat(sChar)
+    }
 
+    if (confirmNC) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (cconfirmL) {
+      passwordCharacters = passwordCharacters.concat(lChar)
+    }
 
-// function prompts() {
-//   confirm(("Please enter a length of at least 8 characters and no more than 128 characters.", 8 < 128))
-//   //(isNaN(inputValue) || inputValue < 8 || inputValue > 128) {
-//     alert("ERROR! Please enter a valid number.")
-//     return false
-//   }}
-//     if (("Would you like your password to include uppercase characters?")) {
-//     inputValue = inputValue.concat(uChar)
-//     }
-//     if (confirm("Would you like your password to include lowercase characters?")) {
-//       inputValue = inputValue.concat(lChar)
-//     }
-//   if (confirm("Would you like your password to include numeric characters?")) {
-//     inputValue=inputValue.concat(nums);
-//   }
-//   if (confirm("Would you like your password to include special characters?")) {
-//     inputValue=inputValue.concat(sChar);
-  }
+    if (confirmU) {
+      passwordCharacters = passwordCharacters.concat(uChar)
+    }
 
-//research confirm statements 
+      console.log(passwordCharacters)
 
+      // Empty string to be filled based on for loop selecting random characters from the array targeted
+      let randomPassword = ""
+      
+      for (let i = 0; i < confirmL; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+}
 
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
-
-
-
- 
-
-
-
-
-
-
-// //// Assignment Code
-// let generateBtn = document.querySelector("#generate");✅
-// // Write password to the #password input✅
-// function generatePassword() {
-//   let password = generatePassword();
-//   let passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", generatePassword);}}}}}}}}}}
-
-
-
-
-
-//let prompt1=prompt("Please enter a length of at least 8 characters and no more than 128 characters.");
-// let prompt2=prompt("Would you like your password to include uppercase characters?");
-// let prompt3=prompt("Would you like your password to include lowercase characters?");
-// let prompt4 =prompt("Would you like your password to include numeric characters?");
-// let  prompt5 =prompt("Would you like your password to include special characters?");
+  passwordText.value = password
+}
